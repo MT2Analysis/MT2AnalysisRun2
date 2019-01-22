418199a3f9207ff2915c47dbab720de76512ce3d
Maria Giulia Ratti <maria.giulia.ratti@cern.ch>
2019-01-22 19:13:48 +0100

    scripts for sanity of estimates

--- analysis/checkLlepEstimates.cpp
+++ analysis/checkLlepEstimates.cpp
@@ -1,35 +1,40 @@
-// Script to check different yields as obtained from two different versions of the code
-// Relative yields difference above specified threshold will be printed to screen
-// example usage:
-// ./checkYields /shome/mratti/mt2_workarea/CMSSW_8_0_12/src/2016_compatible/analysis/EventYields_dataETH_SnTMC_35p9ifb/llepControlRegion/data.root /shome/mratti/mt2_workarea/CMSSW_8_0_12/src/old_mt2analysis/analysis/EventYields_dataETH_SnTMC_35p9ifb/llepControlRegion/data.root llepCR 0.0001
+// Script to check estimates for full run2 stats with two methods
+// old method, all separate
+// new method, all together
+// ./checkLlepEstimates
 
 #include <fstream>
 
 #include "TString.h"
 #include "TCanvas.h"
+#include "TF1.h"
+
 #include "interface/MT2Analysis.h"
 #include "interface/MT2Estimate.h"
 
-int main( int argc, char* argv[] ) {
-
-  if( argc != 6 ) {
-    std::cout << "USAGE: ./checkYields [newYieldsFile] [oldYieldsFile] [newDirName] [oldDirName] [max allowed difference]" << std::endl;
-    std::cout << "Exiting." << std::endl;
-    exit(11);
-  }
+int main( ) {
 
   // new file /shome/mratti/mt2_workarea/CMSSW_8_0_12/src/2016_compatible/analysis/EventYields_dataETH_SnTMC_35p9ifb/llepControlRegion/data.root
   // old file /shome/mratti/mt2_workarea/CMSSW_8_0_12/src/old_mt2analysis/analysis/EventYields_dataETH_SnTMC_35p9ifb/llepControlRegion/data.root
+  float thr = 0.05; // 5 % -> 0.05
 
-  std::string newFileName = argv[1];
-  std::string oldFileName = argv[2];
-  std::string newDirName = argv[3];
-  std::string oldDirName = argv[4];
-  float thr = std::stod(argv[5]);
+  std::string dir16 = "./EventYields_moriond2019_zurich2016_35p9ifb/";
+  std::string dir17 = "./EventYields_moriond2019_zurich2016_41p9ifb_2017/";
+  std::string dir18 = "./EventYields_moriond2019_zurich2016_59p9ifb_2018/";
 
-  MT2Analysis<MT2Estimate>* newEst  = MT2Analysis<MT2Estimate>::readFromFile( newFileName, newDirName ); // second argument is the name of the directory
-  MT2Analysis<MT2Estimate>* oldEst  = MT2Analysis<MT2Estimate>::readFromFile( oldFileName, oldDirName );
-  std::cout << "Read in all the inputs" << std::endl;
+  // new way of calculating bkg
+  MT2Analysis<MT2Estimate>* newEst    = MT2Analysis<MT2Estimate>::readFromFile( dir16 + "llepEstimateCombined.root", "llepEstimate16" );
+  MT2Analysis<MT2Estimate>* newEst17  = MT2Analysis<MT2Estimate>::readFromFile( dir16 + "llepEstimateCombined.root", "llepEstimate17" );
+  MT2Analysis<MT2Estimate>* newEst18  = MT2Analysis<MT2Estimate>::readFromFile( dir16 + "llepEstimateCombined.root", "llepEstimate18" );
+  (*newEst) += (*(newEst17));
+  (*newEst) += (*(newEst18));
+
+  // old way of calculating bkg
+  MT2Analysis<MT2Estimate>* oldEst  = MT2Analysis<MT2Estimate>::readFromFile( dir16 + "llepEstimate.root", "llepEstimate" );
+  MT2Analysis<MT2Estimate>* oldEst17  = MT2Analysis<MT2Estimate>::readFromFile( dir17 + "llepEstimate.root", "llepEstimate" );
+  MT2Analysis<MT2Estimate>* oldEst18  = MT2Analysis<MT2Estimate>::readFromFile( dir18 + "llepEstimate.root", "llepEstimate" );
+  (*oldEst) += (*(oldEst17));
+  (*oldEst) += (*(oldEst18));
 
   // got the estimates
   std::set<MT2Region> regions = newEst->getRegions();
@@ -37,7 +42,7 @@ int main( int argc, char* argv[] ) {
   std::cout << "Defined Regions " << std::endl;
 
   // a plot to look overall at the differences
-  TH1D* hDiff = new TH1D("hDiff", "hDiff", 50,-100., 100.);
+  TH1D* hDiff = new TH1D("hDiff", "hDiff", 100,-50., 50.);
 
   // loop over topological regions
   for( std::set<MT2Region>::iterator iR=regions.begin(); iR!=regions.end(); ++iR ) {
@@ -66,13 +71,13 @@ int main( int argc, char* argv[] ) {
       //  std::cout << "New="  << this_new_yield << " Old="  << this_old_yield << std::endl;
       //  std::cout << "  Diff=" << diff*100 << "%" << std::endl;
       //}
-
+      if (abs(diff) >= thr) {
         std::cout << "  iBin=["  << this_new_TopoYield->GetBinLowEdge(iBin) << ","
                                << this_new_TopoYield->GetBinLowEdge(iBin+1) << "]" 
         
                   << "  New="  << this_new_yield << " Old="  << this_old_yield 
                   << "  Diff=" << diff*100 << "%" << std::endl;
-
+      }
 
     }
     std::cout << std::endl;
@@ -80,7 +85,10 @@ int main( int argc, char* argv[] ) {
 
   TCanvas *c = new TCanvas("c","c");
   hDiff->Draw("hist");
-  c->SaveAs("diff.pdf");
+  TF1 *f1 = new TF1("f1", "gaus", -50, 50);
+  hDiff->Fit("f1", "SRM");
+  f1->Draw("same");
+  c->SaveAs("diffLlepEst.pdf");
 
   return 0;
 
