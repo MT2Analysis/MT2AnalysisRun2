8664c79c6e8fc3ddfd1da05e0fac9abfdcc0f2ce
Maria Giulia Ratti <maria.giulia.ratti@cern.ch>
2019-01-22 13:45:36 +0100

    continued adaptation of computeLostLepton for combination

--- analysis/computeLostLepton_combined.cpp
+++ analysis/computeLostLepton_combined.cpp
@@ -1,5 +1,6 @@
-// a generalization of computeLostLepton to be run three times for each year
+// A generalization of computeLostLepton to estimate the lost lepton background when combining three years
 // must be run with three config files
+// output will be saved in the dir specified in the first config file
 
 #include <iostream>
 #include <sstream>
@@ -27,7 +28,8 @@
 #include "TLorentzVector.h"
 
 
-
+// FIXME: remove duplicates  hybrid_llepCR16 , 17, 18
+// FIXME: remove duplicates for extrapol_bin16
 
 void buildHybrid( MT2Analysis<MT2Estimate>* hybrid_llepCR, MT2Analysis<MT2Estimate>* hybrid_shape, MT2Analysis<MT2Estimate>* data_forShape, MT2Analysis<MT2Estimate>* MCsr_forShape, MT2Analysis<MT2Estimate>* MCcr_forShape, MT2Analysis<MT2Estimate>* bin_extrapol );
 
@@ -65,9 +67,9 @@ int main( int argc, char* argv[] ) {
   TH1::AddDirectory(kTRUE);
 
 
-  std::string llepControlRegionDir1 = cfg16.getEventYieldDir() + "/llepControlRegion";
-  std::string llepControlRegionDir2 = cfg17.getEventYieldDir() + "/llepControlRegion";
-  std::string llepControlRegionDir3 = cfg18.getEventYieldDir() + "/llepControlRegion";
+  std::string llepControlRegionDir16 = cfg16.getEventYieldDir() + "/llepControlRegion";
+  std::string llepControlRegionDir17 = cfg17.getEventYieldDir() + "/llepControlRegion";
+  std::string llepControlRegionDir18 = cfg18.getEventYieldDir() + "/llepControlRegion";
 
 
   ////////////////////////////////////////////////////////////////
@@ -75,103 +77,183 @@ int main( int argc, char* argv[] ) {
   ////////////////////////////////////////////////////////////////
 
   // Data Control Region
-  MT2Analysis<MT2Estimate>* llepCR16 = MT2Analysis<MT2Estimate>::readFromFile(llepControlRegionDir1 + "/data.root", "llepCR");
-  llepCR16->setName("llepCR16");
-  MT2Analysis<MT2Estimate>* llepCR17 = MT2Analysis<MT2Estimate>::readFromFile(llepControlRegionDir2 + "/data.root", "llepCR");
-  llepCR17->setName("llepCR17");
-  MT2Analysis<MT2Estimate>* llepCR18 = MT2Analysis<MT2Estimate>::readFromFile(llepControlRegionDir3 + "/data.root", "llepCR");
-  llepCR18->setName("llepCR18");
+  MT2Analysis<MT2Estimate>* dataCR16 = MT2Analysis<MT2Estimate>::readFromFile(llepControlRegionDir16 + "/data.root", "llepCR");
+  dataCR16->setName("dataCR16");
+  MT2Analysis<MT2Estimate>* dataCR17 = MT2Analysis<MT2Estimate>::readFromFile(llepControlRegionDir17 + "/data.root", "llepCR");
+  dataCR17->setName("dataCR17");
+  MT2Analysis<MT2Estimate>* dataCR18 = MT2Analysis<MT2Estimate>::readFromFile(llepControlRegionDir18 + "/data.root", "llepCR");
+  dataCR18->setName("dataCR18");
 
 
-  if( llepCR16==0 || llepCR17==0 || llepCR18==0 ) {
-    std::cout << "-> Please run llepControlRegion first. I need to get the llepCR yields from there." << std::endl;
+  if( dataCR16==0 || dataCR17==0 || dataCR18==0 ) {
+    std::cout << "-> Please run llepControlRegion first. I need to get the dataCR yields from there." << std::endl;
     std::cout << "-> Thank you for your cooperation." << std::endl;
     exit(193);
   }
 
   // build sum from the first
-  //MT2Analysis<MT2Estimate>* llepCR = MT2Analysis<MT2Estimate>::readFromFile(llepControlRegionDir1 + "/data.root", "llepCR");
-  MT2Analysis<MT2Estimate>* llepCR = new MT2Analysis<MT2Estimate>( *(llepCR16) );
-  llepCR->setName("llepCR");
-  (*llepCR) += (*(llepCR17));
-  (*llepCR) += (*(llepCR18));
+  //MT2Analysis<MT2Estimate>* dataCR = MT2Analysis<MT2Estimate>::readFromFile(llepControlRegionDir16 + "/data.root", "dataCR");
+  MT2Analysis<MT2Estimate>* dataCR = new MT2Analysis<MT2Estimate>( *(dataCR16) );
+  dataCR->setName("dataCR");
+  (*dataCR) += (*(dataCR17));
+  (*dataCR) += (*(dataCR18));
 
-/*
-  // MC Control Region
-  MT2Analysis<MT2Estimate>* MCcr_ = MT2Analysis<MT2Estimate>::readFromFile(llepControlRegionDir1 + "/mc.root", "llepCR");
-  if( MCcr_==0 ) {
-    std::cout << "-> Please run llepControlRegion first. I need to get the MC llepCR yields from there." << std::endl;
-    std::cout << "-> Thank you for your cooperation." << std::endl;
-    exit(193);
-  }
 
-  // MC Control Region Integral
-  MT2Analysis<MT2Estimate>* MCcr = MT2Analysis<MT2Estimate>::readFromFile(llepControlRegionDir1 + "/mc.root", "llepCR");
-  MCcr->setName("llepCR_mc");
+  // MC Control Region combined and normalized to lumi - needed for MC stat uncertainty
+  MT2Analysis<MT2Estimate>* MCcr16 = MT2Analysis<MT2Estimate>::readFromFile(llepControlRegionDir16 + "/mc.root", "llepCR");
+  MCcr16->setName("MCcr16");
+  (*MCcr16) *= cfg16.lumi();
+
+  MT2Analysis<MT2Estimate>* MCcr17 = MT2Analysis<MT2Estimate>::readFromFile(llepControlRegionDir17 + "/mc.root", "llepCR");
+  MCcr17->setName("MCcr17");
+  (*MCcr17) *= cfg17.lumi();
+
+  MT2Analysis<MT2Estimate>* MCcr18 = MT2Analysis<MT2Estimate>::readFromFile(llepControlRegionDir18 + "/mc.root", "llepCR");
+  MCcr18->setName("MCcr18");
+  (*MCcr18) *= cfg18.lumi();
+
+  MT2Analysis<MT2Estimate>* MCcr = new MT2Analysis<MT2Estimate>( *(MCcr16) );
+  MCcr->setName("MCcr");
+  (*MCcr) += (*(MCcr17));
+  (*MCcr) += (*(MCcr18));
+
+  // MC Signal Region - separate - needed for MC stat unceratinty
+  MT2Analysis<MT2Estimate>* MCsr16 = MT2Analysis<MT2Estimate>::readFromFile(cfg16.getEventYieldDir() + "/analyses.root", "Top");
+  MCsr16->setName( "MCsr16" );
+  MT2Analysis<MT2Estimate>* WJets16 = MT2Analysis<MT2Estimate>::readFromFile(cfg16.getEventYieldDir() + "/analyses.root", "WJets");
+  (*MCsr16) += (*(WJets16));
+  (*MCsr16) *= cfg16.lumi();
+
+  MT2Analysis<MT2Estimate>* MCsr17 = MT2Analysis<MT2Estimate>::readFromFile(cfg17.getEventYieldDir() + "/analyses.root", "Top");
+  MCsr17->setName( "MCsr17" );
+  MT2Analysis<MT2Estimate>* WJets17 = MT2Analysis<MT2Estimate>::readFromFile(cfg17.getEventYieldDir() + "/analyses.root", "WJets");
+  (*MCsr17) += (*(WJets17));
+  (*MCsr17) *= cfg17.lumi();
+
+  MT2Analysis<MT2Estimate>* MCsr18 = MT2Analysis<MT2Estimate>::readFromFile(cfg18.getEventYieldDir() + "/analyses.root", "Top");
+  MCsr18->setName( "MCsr18" );
+  MT2Analysis<MT2Estimate>* WJets18 = MT2Analysis<MT2Estimate>::readFromFile(cfg18.getEventYieldDir() + "/analyses.root", "WJets");
+  (*MCsr18) += (*(WJets18));
+  (*MCsr18) *= cfg18.lumi();
+
+
+  // MC Control Region Integral - combined - needed for RatioMC
+  MT2Analysis<MT2Estimate>* MCcr_ = new MT2Analysis<MT2Estimate>( *(MCcr) );
+  MCcr_->setName("MCcr_");
+
   MT2Analysis<MT2Estimate>* MCcr_integral;
-  MCcr_integral = MT2Estimate::makeIntegralAnalysisFromEstimate( "MCcr_integral", cfg.regionsSet(), MCcr_ );
-  MCcr_integral->setName("llepCR_mc_integral");
+  MCcr_integral = MT2Estimate::makeIntegralAnalysisFromEstimate( "MCcr_integral", cfg16.regionsSet(), MCcr_ );
+  MCcr_integral->setName("MCcr_integral");
 
-  // MC Signal Region
-  MT2Analysis<MT2Estimate>* Top   = MT2Analysis<MT2Estimate>::readFromFile(cfg.getEventYieldDir() + "/analyses.root", "Top");
-  MT2Analysis<MT2Estimate>* WJets = MT2Analysis<MT2Estimate>::readFromFile(cfg.getEventYieldDir() + "/analyses.root", "WJets");
-  if( Top==0 || WJets==0 ) {
-    std::cout << "-> Please run regionEventYields on MC first. I need to get the Top and W+jets MC yields from there." << std::endl;
-    std::cout << "-> Thank you for your cooperation." << std::endl;
-    exit(197);
-  }
-  MT2Analysis<MT2Estimate>* MCsr = new MT2Analysis<MT2Estimate>( *(Top) );
-  (*MCsr) += (*(WJets));
-  MCsr->setName("Top + W+jets");
+  // MC Signal Region Integral - year by year - needed for RatioMC
+  MT2Analysis<MT2Estimate>* MCsr16_ = new MT2Analysis<MT2Estimate>( *(MCsr16) );
+  MCsr16_->setName("MCsr16_");
+  MT2Analysis<MT2Estimate>* MCsr16_integral;
+  MCsr16_integral = MT2Estimate::makeIntegralAnalysisFromEstimate( "MCsr16_integral", cfg16.regionsSet(), MCsr16_ );
+  MCsr16_integral->setName("MCsr16_integral");
 
-  // MC Signal Region integral
-  MT2Analysis<MT2Estimate>* MCsr_   = MT2Analysis<MT2Estimate>::readFromFile(cfg.getEventYieldDir() + "/analyses.root", "Top");
-  MCsr_->setName("MCsr_");
+  MT2Analysis<MT2Estimate>* MCsr17_ = new MT2Analysis<MT2Estimate>( *(MCsr17) );
+  MCsr17_->setName("MCsr17_");
+  MT2Analysis<MT2Estimate>* MCsr17_integral;
+  MCsr17_integral = MT2Estimate::makeIntegralAnalysisFromEstimate( "MCsr17_integral", cfg17.regionsSet(), MCsr17_ );
+  MCsr17_integral->setName("MCsr17_integral");
 
-  MT2Analysis<MT2Estimate>* WJets_ = MT2Analysis<MT2Estimate>::readFromFile(cfg.getEventYieldDir() + "/analyses.root", "WJets");
-  (*MCsr_) += (*(WJets_));
+  MT2Analysis<MT2Estimate>* MCsr18_ = new MT2Analysis<MT2Estimate>( *(MCsr18) );
+  MCsr18_->setName("MCsr18_");
+  MT2Analysis<MT2Estimate>* MCsr18_integral;
+  MCsr18_integral = MT2Estimate::makeIntegralAnalysisFromEstimate( "MCsr18_integral", cfg18.regionsSet(), MCsr18_ );
+  MCsr18_integral->setName("MCsr18_integral");
 
-  MT2Analysis<MT2Estimate>* MCsr_integral;
-  MCsr_integral = MT2Estimate::makeIntegralAnalysisFromEstimate( "MCsr_integral", cfg.regionsSet(), MCsr_ );
+  // MC ratio SR/CR - year by year
+  MT2Analysis<MT2Estimate>* RatioMC16 = new MT2Analysis<MT2Estimate>( "RatioMC16", cfg16.regionsSet() );
+  (*RatioMC16) = ( (*MCsr16_integral) / (*MCcr_integral) );
 
+  MT2Analysis<MT2Estimate>* RatioMC17 = new MT2Analysis<MT2Estimate>( "RatioMC17", cfg17.regionsSet() );
+  (*RatioMC17) = ( (*MCsr17_integral) / (*MCcr_integral) );
 
-  // MC ratio SR/CR
-  MT2Analysis<MT2Estimate>* RatioMC = new MT2Analysis<MT2Estimate>( "llepRatioMC", cfg.regionsSet() );
-  (*RatioMC) = ( (*MCsr_integral) / (*MCcr_integral) );
-
+  MT2Analysis<MT2Estimate>* RatioMC18 = new MT2Analysis<MT2Estimate>( "RatioMC18", cfg18.regionsSet() );
+  (*RatioMC18) = ( (*MCsr18_integral) / (*MCcr_integral) );
 
   ////////////////////////////////////////////////////////////////
   // Prepare estimates for Hybrid shape calculation
   ////////////////////////////////////////////////////////////////
 
-  // Hybrid shapes that will be computed
-  MT2Analysis<MT2Estimate>* hybrid_shape = new MT2Analysis<MT2Estimate>( "hybrid_shape", cfg.regionsSet() );
-  MT2Analysis<MT2Estimate>* hybrid_llepCR = new MT2Analysis<MT2Estimate>( "hybrid_llepCR", cfg.regionsSet() );
+  // Hybrid shapes that will be computed - hybrid_llepCR should be the same for the three cases
+  //                                       hybrid_shape  is going to be different in the numerator (using year-by-year MC)
 
-  // extrapol bin that will be computed
-  MT2Analysis<MT2Estimate>* extrapol_bin = new MT2Analysis<MT2Estimate>( "extrapol_bin", cfg.regionsSet() );
+  MT2Analysis<MT2Estimate>* hybrid_shape16 = new MT2Analysis<MT2Estimate>( "hybrid_shape16", cfg16.regionsSet() );
+  MT2Analysis<MT2Estimate>* hybrid_shape17 = new MT2Analysis<MT2Estimate>( "hybrid_shape17", cfg17.regionsSet() );
+  MT2Analysis<MT2Estimate>* hybrid_shape18 = new MT2Analysis<MT2Estimate>( "hybrid_shape18", cfg18.regionsSet() );
 
-  // MC Signal Region (multiplied by lumi)
-  MT2Analysis<MT2Estimate>* MCsr_forShape   = MT2Analysis<MT2Estimate>::readFromFile(cfg.getEventYieldDir() + "/analyses.root", "Top");
-  MCsr_forShape->setName( "MCsr_forShape" );
-  MT2Analysis<MT2Estimate>* WJets_forShape = MT2Analysis<MT2Estimate>::readFromFile(cfg.getEventYieldDir() + "/analyses.root", "WJets");
-  (*MCsr_forShape) += (*(WJets_forShape));
+  // for the moment keep three separate for sanity checks
+  MT2Analysis<MT2Estimate>* hybrid_llepCR16 = new MT2Analysis<MT2Estimate>( "hybrid_llepCR16", cfg16.regionsSet() );
+  MT2Analysis<MT2Estimate>* hybrid_llepCR17 = new MT2Analysis<MT2Estimate>( "hybrid_llepCR17", cfg17.regionsSet() );
+  MT2Analysis<MT2Estimate>* hybrid_llepCR18 = new MT2Analysis<MT2Estimate>( "hybrid_llepCR18", cfg18.regionsSet() );
 
-  // MC Control Region (multiplied by lumi)
-  MT2Analysis<MT2Estimate>*  MCcr_forShape = MT2Analysis<MT2Estimate>::readFromFile(llepControlRegionDir1 + "/mc.root", "llepCR");
-  MCcr_forShape->setName( "MCcr_forShape" );
+  // extrapol bin that will be computed - only one for all three years
+  MT2Analysis<MT2Estimate>* extrapol_bin16 = new MT2Analysis<MT2Estimate>( "extrapol_bin16", cfg16.regionsSet() );
+  MT2Analysis<MT2Estimate>* extrapol_bin17 = new MT2Analysis<MT2Estimate>( "extrapol_bin17", cfg17.regionsSet() );
+  MT2Analysis<MT2Estimate>* extrapol_bin18 = new MT2Analysis<MT2Estimate>( "extrapol_bin18", cfg18.regionsSet() );
 
-  (*MCsr_forShape) *= cfg.lumi();
-  (*MCcr_forShape) *= cfg.lumi();
+  // MC Signal Region (multiplied by lumi) - one for each year
+  MT2Analysis<MT2Estimate>* MCsr_forShape16   = MT2Analysis<MT2Estimate>::readFromFile(cfg16.getEventYieldDir() + "/analyses.root", "Top");
+  MCsr_forShape16->setName( "MCsr_forShape16" );
+  MT2Analysis<MT2Estimate>* WJets_forShape16 = MT2Analysis<MT2Estimate>::readFromFile(cfg16.getEventYieldDir() + "/analyses.root", "WJets");
+  (*MCsr_forShape16) += (*(WJets_forShape16));
+  (*MCsr_forShape16) *= cfg16.lumi();
 
-  // Data control region (multiplied by lumi)
-  MT2Analysis<MT2Estimate>* llepCR_data_forShape;
-  llepCR_data_forShape  = MT2Analysis<MT2Estimate>::readFromFile(llepControlRegionDir1 + "/data.root", "llepCR");
+  MT2Analysis<MT2Estimate>* MCsr_forShape17   = MT2Analysis<MT2Estimate>::readFromFile(cfg17.getEventYieldDir() + "/analyses.root", "Top");
+  MCsr_forShape17->setName( "MCsr_forShape17" );
+  MT2Analysis<MT2Estimate>* WJets_forShape17 = MT2Analysis<MT2Estimate>::readFromFile(cfg17.getEventYieldDir() + "/analyses.root", "WJets");
+  (*MCsr_forShape17) += (*(WJets_forShape17));
+  (*MCsr_forShape17) *= cfg17.lumi();
 
+  MT2Analysis<MT2Estimate>* MCsr_forShape18   = MT2Analysis<MT2Estimate>::readFromFile(cfg18.getEventYieldDir() + "/analyses.root", "Top");
+  MCsr_forShape18->setName( "MCsr_forShape18" );
+  MT2Analysis<MT2Estimate>* WJets_forShape18 = MT2Analysis<MT2Estimate>::readFromFile(cfg18.getEventYieldDir() + "/analyses.root", "WJets");
+  (*MCsr_forShape18) += (*(WJets_forShape18));
+  (*MCsr_forShape18) *= cfg18.lumi();
 
+  // MC Control Region (multiplied by lumi) - one for each year and then combined
+  MT2Analysis<MT2Estimate>*  MCcr_forShape16 = MT2Analysis<MT2Estimate>::readFromFile(llepControlRegionDir16 + "/mc.root", "llepCR");
+  MCcr_forShape16->setName( "MCcr_forShape16" );
+  (*MCcr_forShape16) *= cfg16.lumi();
 
-  //Building the hybrid shape
-  buildHybrid( hybrid_llepCR, hybrid_shape, llepCR_data_forShape, MCsr_forShape, MCcr_forShape, extrapol_bin );
+  MT2Analysis<MT2Estimate>*  MCcr_forShape17 = MT2Analysis<MT2Estimate>::readFromFile(llepControlRegionDir17 + "/mc.root", "llepCR");
+  MCcr_forShape17->setName( "MCcr_forShape17" );
+  (*MCcr_forShape17) *= cfg17.lumi();
+
+  MT2Analysis<MT2Estimate>*  MCcr_forShape18 = MT2Analysis<MT2Estimate>::readFromFile(llepControlRegionDir18 + "/mc.root", "llepCR");
+  MCcr_forShape18->setName( "MCcr_forShape18" );
+  (*MCcr_forShape18) *= cfg18.lumi();
+
+  MT2Analysis<MT2Estimate>*  MCcr_forShape = new MT2Analysis<MT2Estimate>( *(MCcr_forShape16) );
+  MCcr_forShape->setName("MCcr_forShape");
+  (*MCcr_forShape) += (*(MCcr_forShape17));
+  (*MCcr_forShape) += (*(MCcr_forShape18));
+
+  // Data control region - one for each year and then combined
+  MT2Analysis<MT2Estimate>* llepCR_data_forShape16;
+  llepCR_data_forShape16  = MT2Analysis<MT2Estimate>::readFromFile(llepControlRegionDir16 + "/data.root", "llepCR");
+  llepCR_data_forShape16->setName("llepCR_data_forShape16");
+
+  MT2Analysis<MT2Estimate>* llepCR_data_forShape17;
+  llepCR_data_forShape17  = MT2Analysis<MT2Estimate>::readFromFile(llepControlRegionDir17 + "/data.root", "llepCR");
+  llepCR_data_forShape17->setName("llepCR_data_forShape17");
+
+  MT2Analysis<MT2Estimate>* llepCR_data_forShape18;
+  llepCR_data_forShape18  = MT2Analysis<MT2Estimate>::readFromFile(llepControlRegionDir18 + "/data.root", "llepCR");
+  llepCR_data_forShape18->setName("llepCR_data_forShape18");
+
+  MT2Analysis<MT2Estimate>*  llepCR_data_forShape = new MT2Analysis<MT2Estimate>( *(llepCR_data_forShape16) );
+  llepCR_data_forShape->setName("llepCR_data_forShape");
+  (*llepCR_data_forShape) += (*(llepCR_data_forShape17));
+  (*llepCR_data_forShape) += (*(llepCR_data_forShape18));
+
+  //Building the hybrid shapes
+  buildHybrid( hybrid_llepCR16, hybrid_shape16, llepCR_data_forShape, MCsr_forShape16, MCcr_forShape, extrapol_bin16 );
+  buildHybrid( hybrid_llepCR17, hybrid_shape17, llepCR_data_forShape, MCsr_forShape17, MCcr_forShape, extrapol_bin17 );
+  buildHybrid( hybrid_llepCR18, hybrid_shape18, llepCR_data_forShape, MCsr_forShape18, MCcr_forShape, extrapol_bin18 );
   // final estimate will be hybrid_llepCR * hybrid_shape
 
   ////////////////////////////////////////////////////////////////
@@ -179,22 +261,56 @@ int main( int argc, char* argv[] ) {
   ////////////////////////////////////////////////////////////////
 
   // Lost lepton estimate
-  MT2Analysis<MT2Estimate>* llepEstimate = new MT2Analysis<MT2Estimate>( "llepEstimate", cfg.regionsSet() );
-  (*llepEstimate) = (*hybrid_llepCR) * (*hybrid_shape);
-*/
-  std::string outFile = cfg16.getEventYieldDir() + "/llepEstimate";
+  MT2Analysis<MT2Estimate>* llepEstimate16 = new MT2Analysis<MT2Estimate>( "llepEstimate16", cfg16.regionsSet() );
+  (*llepEstimate16) = (*hybrid_llepCR16) * (*hybrid_shape16);
+
+  MT2Analysis<MT2Estimate>* llepEstimate17 = new MT2Analysis<MT2Estimate>( "llepEstimate17", cfg17.regionsSet() );
+  (*llepEstimate17) = (*hybrid_llepCR17) * (*hybrid_shape17);
+
+  MT2Analysis<MT2Estimate>* llepEstimate18 = new MT2Analysis<MT2Estimate>( "llepEstimate18", cfg18.regionsSet() );
+  (*llepEstimate18) = (*hybrid_llepCR18) * (*hybrid_shape18);
+
+  // save everything in the first directory ...
+  std::string outFile = cfg16.getEventYieldDir() + "/llepEstimateCombined";
   outFile += ".root";
 
-  llepCR->writeToFile( outFile, "recreate" );
-  //llepCR16->addToFile( outFile );
-  //llepCR17->addToFile( outFile );
-  //llepCR18->addToFile( outFile );
 
-  //llepEstimate->writeToFile( outFile, "recreate" );
-  //hybrid_shape->addToFile( outFile );
-  //hybrid_llepCR->addToFile( outFile );
-  //extrapol_bin->addToFile( outFile );
+
+  llepEstimate16->writeToFile( outFile, "recreate" );
+  llepEstimate17->addToFile( outFile );
+  llepEstimate18->addToFile( outFile );
+
+  hybrid_shape16->addToFile( outFile );
+  hybrid_shape17->addToFile( outFile );
+  hybrid_shape18->addToFile( outFile );
+
+  hybrid_llepCR16->addToFile( outFile ); // these two should be the same
+  hybrid_llepCR17->addToFile( outFile ); // these two should be the same
+  hybrid_llepCR18->addToFile( outFile ); // these two should be the same
+
+  extrapol_bin16->addToFile( outFile ); // these two should be the same
+  extrapol_bin17->addToFile( outFile ); // these two should be the same
+  extrapol_bin18->addToFile( outFile ); // these two should be the same
   //RatioMC->addToFile( outFile );
+
+  MCcr->addToFile(outFile);
+  MCcr16->addToFile(outFile);
+  MCcr17->addToFile(outFile);
+  MCcr18->addToFile(outFile);
+
+  MCsr16->addToFile(outFile);
+  MCsr17->addToFile(outFile);
+  MCsr18->addToFile(outFile);
+
+  RatioMC16->addToFile(outFile);
+  RatioMC17->addToFile(outFile);
+  RatioMC18->addToFile(outFile);
+
+  dataCR->addToFile( outFile );
+  dataCR16->addToFile( outFile );
+  dataCR17->addToFile( outFile );
+  dataCR18->addToFile( outFile );
+
 
   //MCsr->addToFile( outFile );
   //llep_est_integral->addToFile( outFile );
